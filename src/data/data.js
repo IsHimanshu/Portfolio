6eabfef90a25ad7c495070461c2bcbf3ba2ed65e
ishimanshu <himanshus4965@gmail.com>
2023-12-14 21:56:18 +0530

    addedprojects

--- src/data/data.js
+++ src/data/data.js
@@ -11,18 +11,18 @@ export const data = [
   },
   {
     id: 2,
-    img: 'https://i.postimg.cc/Bv75CLyN/4167022.jpg',
-    title: "Tarvel Agency",
-    link: "",
-    git: "",
+    img: 'https://i.postimg.cc/9MD6TfbC/2023-12-14-21-54.png',
+    title: "Anime Recommendation System",
+    link: "himanshusrivastva.me/Projects/AnimeRecommendation",
+    git: "https://github.com/IsHimanshu/AnimeRecommendation",
   },
   {
     id: 3,
-    img: 'https://i.postimg.cc/wBjcJcW7/5890625.jpg',
-    title: "Music Player",
+    img: 'https://i.postimg.cc/tg5t9tGM/2023-12-14-21-38.png',
+    title: "CriSys(Disaster Management Community",
     desc: "",
-    link: "",
-    git: "",
+    link: "https://crisys-eight.vercel.app/",
+    git: "https://github.com/IsHimanshu/CriSys",
   }
   
 ];
